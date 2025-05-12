e99291592f2ebb8d2a8fb684ac24efef07f34773
Nouster <djebali.mohamed88@gmail.com>
2025-05-12 17:17:47 +0200

    update test

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-def hello(name="GitHub Actions"):
-    return "Hello, " + name + "!"
+def hello(firstname="GitHub", lastname="Actions"):
+    if not isinstance(firstname, str) or not isinstance(lastname, str):
+        raise TypeError("Format de nom invalide. Veuillez entrer une chaîne de caractères.")
+
+    # Si un seul argument est passé, on peut donc supposer que lastname n’est pas utilisé
+    if lastname == "Actions" and firstname != "GitHub":
+        return f"Hello, {firstname}!"
+    
+    return f"Hello, {firstname} {lastname}!"
--- test_main.py
+++ test_main.py
@@ -1,8 +1,23 @@
+import pytest
+import time
 from main import hello
 
 def test_hello():
     assert hello() == "Hello, GitHub Actions!"
-    
-def test_hello_custom_name ( ):
-    
-    assert hello ("EPSI") == "Hello, EPSI!"
+
+def test_hello_custom_name():
+    assert hello("EPSI") == "Hello, EPSI!"
+
+def test_hello_chaine_invalide():
+    with pytest.raises(TypeError):
+        hello(7878567)
+
+def test_hello_performance():
+  start = time.time()
+  for _ in range(1000):
+    hello("EPSI")
+  duration = time.time() - start
+  assert duration < 1
+  
+def test_hello_full_name():
+    assert hello("Jane", "Smith") == "Hello, Jane Smith!"
